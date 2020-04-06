913165ad53fc9d40ef723d9ae09df61be95464c0
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-04-06 18:00:03 +0700

    Lifecycle method of react js

---
+++ src/LifecycleMethods.js
@@ -0,0 +1,22 @@
+class App extends React.Component {
+    // whenever the component called
+    constructur() {}
+
+    // it is a must to declare this method when create a class based component
+    render() {}
+
+    // content visible on the screen
+    componentDidMount() {}
+
+    // sit and wait for updates
+    componentDidUpdate() {}
+
+    // sit and wait until this component isn't longer show
+    componentWillUnmount() {}
+
+    shouldComponentUpdate() {}
+
+    getDrivedStateFromProps() {}
+
+    getSnapshotBeforeUpdate() {}
+}
